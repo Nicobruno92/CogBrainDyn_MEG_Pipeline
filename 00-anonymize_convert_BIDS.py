10fe4de4c347bc9627e81e660f5ca73f317dfae4
Sophie Herbst <ksherbst@gmail.com>
2019-11-08 14:41:22 +0100

    add event ids

--- 00-anonymize_convert_BIDS.py
+++ 00-anonymize_convert_BIDS.py
@@ -2,7 +2,14 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Nov  7 11:54:21 2019
-Read raws, anonimize and save as BIDS
+
+This script takes the raw data and sets up a BIDS structure in a specified 
+repository (bids_root).
+
+It reads values from the study config file. 
+
+Have a look at the documentation here: https://mne.tools/mne-bids/index.html
+
 @author: sh254795
 """
 
@@ -18,13 +25,15 @@ from mne_bids import write_raw_bids, make_bids_basename, write_anat
 from datetime import datetime
 import re
 
+import config
+
 bids_root = '/neurospin/meg/meg_tmp/Dynacomp_Ciuciu_2011/2019_MEG_Pipeline/BIDS/'
 subjects_dir = '/neurospin/meg/meg_tmp/Dynacomp_Ciuciu_2011/2019_MEG_Pipeline/MRI/'
 trans_dir =  '/neurospin/meg/meg_tmp/Dynacomp_Ciuciu_2011/2019_MEG_Pipeline/MEG/trans/'
 
 base_path = '/neurospin/meg/meg_tmp/Dynacomp_Ciuciu_2011/2019_MEG_Pipeline/MEG/'
-# subjects = ['SB01', 'SB02','SB03','SB04','SB05','SB06','SB07','SB08','SB09','SB10','SB11','SB12']
-subjects = ['SB01', 'SB02','SB04']
+subjects = ['SB01', 'SB02','SB03','SB04','SB05','SB06','SB07','SB08','SB09','SB10','SB11','SB12']
+# subjects = ['SB02']
 
 tasks = ['Localizer','empty'] # ,'empty'
 
@@ -76,14 +85,19 @@ for ss, subject in enumerate(subjects):
                            overwrite=True) 
         
         else:
-            events = mne.find_events(raw, min_duration=0.002, initial_event=True)
-            write_raw_bids(raw, bids_basename,
+          events = mne.find_events(raw, stim_channel=config.stim_channel,
+                                 consecutive=True,
+                                 min_duration=config.min_event_duration,
+                                 shortest_event=config.shortest_event)
+         
+          write_raw_bids(raw, bids_basename,
                            output_path=bids_root,
                            events_data=events,
+                           event_id = config.event_id,
                            overwrite=True)
         
-        if task != 'empty':               
-            # Take care of anatomy
-            write_anat(bids_root, subject, t1w, acquisition="t1w",
-                       trans=trans, raw=raw, overwrite=True)        
+#        if task != 'empty':               
+#            # Take care of anatomy
+#            write_anat(bids_root, subject, t1w, acquisition="t1w",
+#                       trans=trans, raw=raw, overwrite=True)        
 
